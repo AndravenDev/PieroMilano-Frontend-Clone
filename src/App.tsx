d077148305e13247a39a550e8907e59257f5dc4f
aivan1 <78039586+aivan1@users.noreply.github.com>
2022-11-01 12:28:28 +0200

    added main info components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./Components/Header";
+import { Header } from "./Components/Header/Header";
+import { MainInfo } from "./Components/MainInfo/MainInfo";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div style={{ height: "800px", backgroundColor: "red" }}></div>
+      <MainInfo />
+      <div style={{ height: "800px", backgroundColor: "red" }}></div>
     </div>
   );
 }
---
+++ src/Components/MainInfo/MainInfo.tsx
@@ -0,0 +1,10 @@
+import styles from "./MainInfo.module.scss";
+
+export const MainInfo = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.section1}></div>
+      <div className={styles.section2}></div>
+    </div>
+  );
+};
